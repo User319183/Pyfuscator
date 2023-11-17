01847aa8eb3404ecb0c6ceb920086babe3cfac53
Sathvik <88944590+User319183@users.noreply.github.com>
2023-11-16 20:52:12 -0500

    Update obfuscated_main.py

--- obfuscated_main.py
+++ obfuscated_main.py
@@ -1,17 +1,48 @@
+opaque_predicates_table = []
+credits = {
+    "Obfuscator": {
+        "Created_By": {
+            "User319183": "Free | Open Source Version",
+            "Should I create a premium version?": "lol probably not b/c I'm lazy",
+            "Join the Discord if your a kool kid": "https://discord.gg/KHJjX3y2B4"
+        },
+        "Features": {
+            "Random_String_Generation": "true",
+            "AES_and_RSA_Encryption": "true",
+            "String_Encryption": "true",
+            "String_Decryption": "true",
+            "Code_Obfuscation": "true",
+            "Name_Obfuscation": "true",
+            "Function_Name_Obfuscation": "true",
+            "String_Obfuscation": "true",
+            "Constant_Obfuscation": "true",
+            "Call_Obfuscation": "true",
+            "Advanced_Obfuscation": {
+                "Code_Flattening": "true",
+                "Proprietary_Logic_Insertion": "true",
+                "Safeguard_Insertion": "true",
+                "Dead_Code_Insertion": "true",
+                "Control_Flow_Flattening": "true",
+                "Code_Substitution": "true",
+                "Dummy_Code_Insertion": "true",
+                "Variable_Renaming": "true"
+            }
+        }
+    }
+}
+char_map = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15, 'g': 16, 'h': 17, 'i': 18, 'j': 19, 'k': 20, 'l': 21, 'm': 22, 'n': 23, 'o': 24, 'p': 25, 'q': 26, 'r': 27, 's': 28, 't': 29, 'u': 30, 'v': 31, 'w': 32, 'x': 33, 'y': 34, 'z': 35, 'A': 36, 'B': 37, 'C': 38, 'D': 39, 'E': 40, 'F': 41, 'G': 42, 'H': 43, 'I': 44, 'J': 45, 'K': 46, 'L': 47, 'M': 48, 'N': 49, 'O': 50, 'P': 51, 'Q': 52, 'R': 53, 'S': 54, 'T': 55, 'U': 56, 'V': 57, 'W': 58, 'X': 59, 'Y': 60, 'Z': 61, '!': 62, '"': 63, '#': 64, '$': 65, '%': 66, '&': 67, "'": 68, '(': 69, ')': 70, '*': 71, '+': 72, ',': 73, '-': 74, '.': 75, '/': 76, ':': 77, ';': 78, '<': 79, '=': 80, '>': 81, '?': 82, '@': 83, '[': 84, '\\': 85, ']': 86, '^': 87, '_': 88, '`': 89, '{': 90, '|': 91, '}': 92, '~': 93, ' ': 94, '\t': 95, '\n': 96, '\r': 97, '\x0b': 98, '\x0c': 99}
+strings_table = []
 
-
-def uvdpmolamn(name):
-    ejljumgson = (1 + 0)
-    uhgbgtdkgr = (2 + 0)
-    (ejljumgson + uhgbgtdkgr)
-    print(f'Hello, {name}!')
-
-def yiwbjnggpu(a, b):
-    ejljumgson = (1 + 0)
-    uhgbgtdkgr = (2 + 0)
-    (ejljumgson + uhgbgtdkgr)
-    return (ejljumgson + uhgbgtdkgr)
-zeutpcvyso = 'World'
-uvdpmolamn(zeutpcvyso)
-gkrggwarct = yiwbjnggpu(5, 10)
-print(f'The sum is: {gkrggwarct}')
+MdrqBApqyJM1mwghlN1fEPaoKIuWyWhjmdcAA63fMTMQQ3jsVgNDVpX4CvAAICIifQQz6tU0L6Y0lcNkCRz8JzSnMjXWr4OW60eC = 1
+WoiVM8GB9W3linx6y31OLOYNsnKySH0H5xqTzpUh3UnrBsu6WVSnannz6pAVvCwngbzT9vKnrxnQER00tZxtjttXkxv4EAGu8iXq = 2
+PD4rWYYLeTbWtMQblSkl1msSsI9ZzWuSMEEt2K1GAPZEuIWgDnZtYwXuyEIXidDnf7dxTaTm5NasaRzaTLyqv2jAhgTIOuY8Wv38 = 3
+for um3gky9BMqUnsG7Kl7wAQv3j4iRMAoVJFKsnG6xAXpFnDobr2CScFzZFTq5KxQne67Ltf10XLgmqEtsmdPEFhgBkDmn4dqDxBfwA in range(0, 10):
+    print(um3gky9BMqUnsG7Kl7wAQv3j4iRMAoVJFKsnG6xAXpFnDobr2CScFzZFTq5KxQne67Ltf10XLgmqEtsmdPEFhgBkDmn4dqDxBfwA)
+    print(MdrqBApqyJM1mwghlN1fEPaoKIuWyWhjmdcAA63fMTMQQ3jsVgNDVpX4CvAAICIifQQz6tU0L6Y0lcNkCRz8JzSnMjXWr4OW60eC)
+    print(WoiVM8GB9W3linx6y31OLOYNsnKySH0H5xqTzpUh3UnrBsu6WVSnannz6pAVvCwngbzT9vKnrxnQER00tZxtjttXkxv4EAGu8iXq)
+    print(PD4rWYYLeTbWtMQblSkl1msSsI9ZzWuSMEEt2K1GAPZEuIWgDnZtYwXuyEIXidDnf7dxTaTm5NasaRzaTLyqv2jAhgTIOuY8Wv38)
+    print('we love fruit')
+(6 + 1)
+for lVa9K4YxKlQzy9K4mZkwaU8JW7P96IwaTziGyB1S4F6c6250ZwVJGLwC04uE8gVAPiws8l8QoiyxIB9LtR9vDxnyi9udcJmyyq7Q in range(7):
+    pass
+(9 + 8)
